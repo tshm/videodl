d0c4f752d41f61f5b3fc94144ac49a9010bb2142
Tosh Satake <2sm@csc.jp>
2018-10-23 09:04:17 +0900

    fix: use log4js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /* global cd, exec, exit */
 require('shelljs/global')
-require('scribe-js')()
-const console = process.console
+const log = require('log4js').getLogger()
 const DRY_RUN = process.env.DRY_RUN || false
-if (DRY_RUN) console.warning('run as DRY_RUN')
+log.level = DRY_RUN ? 'debug' : 'info'
+if (DRY_RUN) log.debug('run as DRY_RUN')
 
 function processArguments () {
   const proc = require('process')
   // change current dir
   if (proc.argv.length > 2) {
-    console.info(`change dir to "${proc.argv[2]}"`)
+    log.info(`change dir to "${proc.argv[2]}"`)
     cd(proc.argv[2])
     return true
   }
-  console.info(`need folder path`)
+  log.info(`need folder path`)
   return false
 }
 
@@ -21,10 +21,10 @@ function run (cmd) {
   return new Promise((resolve, reject) => {
     const exitcode = exec(cmd).code
     if (exitcode === 0) {
-      console.info(`running ${cmd} succeed`)
+      log.info(`running ${cmd} succeed`)
       resolve(exitcode)
     } else {
-      console.info(`running ${cmd} failed`)
+      log.info(`running ${cmd} failed`)
       reject(exitcode)
     }
   })
@@ -32,7 +32,7 @@ function run (cmd) {
 
 function getDownloader () {
   return function execDl (url) {
-    console.info('calling ytdl')
+    log.info('calling ytdl')
     if (DRY_RUN) return run(`echo youtube-dl --no-progress "${url}"`)
     return run(`youtube-dl --no-progress "${url}"`)
   }
@@ -51,21 +51,21 @@ function download (database) {
     .then(ss => {
       const obj = ss.val()
       if (!obj) {
-        console.info('empty list')
+        log.info('empty list')
         return false
       }
       return Promise.all(Object.keys(obj).map(k => {
         const v = obj[ k ]
         if (v.watched) {
-          console.info(`deleting pre-marked item: ${v.title}`)
+          log.info(`deleting pre-marked item: ${v.title}`)
           return database.ref(`videos/${k}`).remove()
         }
-        console.info(`downloading: ${v.title}`)
+        log.info(`downloading: ${v.title}`)
         return execDl(v.url).then(v => {
-          console.info(`execDl success: ${v}`)
+          log.info(`execDl success: ${v}`)
           return database.ref(`videos/${k}/watched`).set(true)
         }).catch(e => {
-          console.error(`videodl: download failed... ${v.title} (${e})`)
+          log.error(`videodl: download failed... ${v.title} (${e})`)
           throw e
         })
       }))
@@ -75,14 +75,14 @@ function download (database) {
 /**   main procedure
  */
 function main () {
-  console.info('starting application ...')
+  log.info('starting application ...')
   if (processArguments()) {
     const database = getData()
     download(database).then(v => {
-      console.info(`exiting: ${v}`)
+      log.info(`exiting: ${v}`)
       exit(0)
     }).catch(e => {
-      console.error(`some failed: ${e}`)
+      log.error(`some failed: ${e}`)
       exit(1)
     })
   }
