1b9097fee3a05c09405a821f78d33a7c99e3fce6
Tosh Satake <2sm@csc.jp>
2020-05-25 09:28:04 +0900

    update libs

---
+++ dl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+PATH=$PATH:~/bin/
+WD=$(pwd)
+nvm use 10
+
+cd $(dirname $0)
+source env.sh
+
+node dist/index.js $WD | yarn pino-elasticsearch --node $LOG
+
+youtube-dl -U
