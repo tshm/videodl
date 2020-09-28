addfc2b547b0ff22302b3efba2f4259c85d84082
Tosh Satake <satake@empathy.co.jp>
2020-09-28 13:55:30 +0900

    misc: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,22 @@
 module.exports = {
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    'eslint:recommended',
-    'prettier/@typescript-eslint',
-    'plugin:@typescript-eslint/eslint-recommended'
-  ],
+  extends: ['plugin:prettier/recommended', 'prettier/@typescript-eslint'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['prettier', '@typescript-eslint'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always']
-  }
+    semi: ['error', 'always'],
+  },
 };
