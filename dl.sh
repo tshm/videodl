e9b5be0cb8d11e6f32b5004c68b9805a6e360fcd
Tosh Satake <2sm@csc.jp>
2020-10-20 10:53:44 +0900

    use asdf implicitly

--- dl.sh
+++ dl.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
 PATH=$PATH:~/bin/
 WD=$(pwd)
-nvm use default
 
 cd $(dirname $0)
-source env.sh
+source .env
 
 node dist/index.js $WD | yarn pino-elasticsearch --node $LOG
 
