592946ae7fe54dda9edba910848c611eec22bf05
Tosh Satake <2sm@csc.jp>
2022-03-11 10:56:19 +0900

    chore: update deps

--- dl.sh
+++ dl.sh
@@ -3,4 +3,5 @@ WD=$(pwd)
 cd $(dirname $0)
 
 docker run --rm --env-file .env -v $WD:/home/node/dl videodl
+# docker run -v $(pwd)/vector.toml:/etc/vector/vector.toml:ro -p 8383:8383 timberio/vector:0.20.0-alpine
 
