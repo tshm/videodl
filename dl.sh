1c1b0369939f659a9b50dd06868b8d5c7613d62d
Tosh Satake <2sm@csc.jp>
2022-04-06 08:27:49 +0900

    feat: use vector for log processing

--- dl.sh
+++ dl.sh
@@ -2,6 +2,9 @@
 WD=$(pwd)
 cd $(dirname $0)
 
-docker run --rm --env-file .env -v $WD:/home/node/dl videodl
-# docker run -v $(pwd)/vector.toml:/etc/vector/vector.toml:ro -p 8383:8383 timberio/vector:0.20.0-alpine
+export $(cat .env | xargs)
 
+docker run --rm --env-file .env -v $WD:/home/node/dl videodl |\
+  vector --config .vector.toml
+
+# docker run -v $(pwd)/vector.toml:/etc/vector/vector.toml:ro -p 8383:8383 timberio/vector:0.20.0-alpine
