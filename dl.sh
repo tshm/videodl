a8b68443359f71aa112e4e19ca583c459c30c022
Tosh Satake <2sm@csc.jp>
2022-06-14 16:50:16 +0900

    fix(log): use vector.yaml

--- dl.sh
+++ dl.sh
@@ -6,6 +6,6 @@ export VECTOR_LOG_FORMAT=json
 export $(cat .env | xargs)
 
 docker run --rm --env-file .env -v $WD:/home/node/dl videodl |\
-  vector --config .vector.toml
+  vector --config .vector.yaml
 
 # docker run -v $(pwd)/vector.toml:/etc/vector/vector.toml:ro -p 8383:8383 timberio/vector:0.20.0-alpine
