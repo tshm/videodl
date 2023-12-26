713ef6254c065f3e2f1a9e616a778a3e67526c09
Tosh Satake <2sm@csc.jp>
2023-12-26 15:33:42 +0900

    split default, local env

--- dl.sh
+++ dl.sh
@@ -5,6 +5,6 @@ cd ${DIR}
 
 export PATH=${PATH}:${DIR}/.devbox/nix/profile/default/bin
 export VECTOR_LOG_FORMAT=json
-export $(cat .env | xargs)
+export $(cat .env.* | xargs)
 
 bun run index.ts ${WD} | vector
